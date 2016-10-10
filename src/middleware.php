78bb20fe1a9cbefcd517be085fb17a482cdb4ee8
falco442 <marcofra83@gmail.com>
2016-10-10 12:24:05 +0200

    Added CORS configurations

--- src/middleware.php
+++ src/middleware.php
@@ -3,3 +3,12 @@
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
 $app->add(new \Slim\Middleware\Auth\TokenAuth);
+
+$app->add(new \Tuupola\Middleware\Cors([
+    "origin" => ["*"],
+    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS"],
+    "headers.allow" => ["Authorization", "If-Match", "If-Unmodified-Since"],
+    "headers.expose" => ["Etag"],
+    "credentials" => true,
+    "cache" => 86400,
+]));
